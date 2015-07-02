78f0676706c96d7bd7bf601e6654c3e552601765
Josh Long <josh@joshlong.com>
2015-07-01 20:05:07 -0500

    updated deploy.sh

--- reservation-client/src/main/java/demo/DemoApplication.java
+++ reservation-client/src/main/java/demo/DemoApplication.java
@@ -58,7 +58,7 @@ class ReservationIntegration {
 @RestController
 class ReservationNamesRestController {
 
-    @RequestMapping("/rs")
+    @RequestMapping("/names")
     Collection<String> rs() {
         return this.reservationIntegration.getReservationNames();
     }
